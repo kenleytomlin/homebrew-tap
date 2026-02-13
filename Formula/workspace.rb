7f04b04f73e76e12873245fefee27fc261f3cf7b
kenleytomlin <3004590+kenleytomlin@users.noreply.github.com>
2026-02-13 00:42:08 +0000

    workspace 0.2.0: add clone and adopt commands

--- Formula/workspace.rb
+++ Formula/workspace.rb
@@ -4,28 +4,28 @@
 class Workspace < Formula
   desc "Buildpacks for agent-friendly git repos"
   homepage "https://github.com/kenleytomlin/workspace-cli"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/kenleytomlin/workspace-cli/releases/download/v#{version}/workspace-darwin-arm64.tar.gz"
-      sha256 "81fc8a4854e817ec612afdef2bd7059c82f7109373c30f6d5eaa64d23ec0420c"
+      sha256 "0c20d8b11b774efb86d9141e56b23f2880507e9893d8050507a1d42fad96397b"
     end
     on_intel do
       url "https://github.com/kenleytomlin/workspace-cli/releases/download/v#{version}/workspace-darwin-x64.tar.gz"
-      sha256 "09312e8d3c2e2449df7db3e33c7b8c2459c6db749ff7bba9918af5833f06005d"
+      sha256 "43629ac82476b30b8ad05f6342cda06c43d7e6838b56f95122385db1d164350b"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/kenleytomlin/workspace-cli/releases/download/v#{version}/workspace-linux-x64.tar.gz"
-      sha256 "cdc3e4f98a984287c28cb18b39af578db177f15096d0ee3793baa99d81e7e34d"
+      sha256 "17237a8b036200b90f273f9765e3bcec7667c9706a554428905161bac900265a"
     end
     on_intel do
       url "https://github.com/kenleytomlin/workspace-cli/releases/download/v#{version}/workspace-linux-x64.tar.gz"
-      sha256 "cdc3e4f98a984287c28cb18b39af578db177f15096d0ee3793baa99d81e7e34d"
+      sha256 "17237a8b036200b90f273f9765e3bcec7667c9706a554428905161bac900265a"
     end
   end
 
